3f8378882682a1bdd75b3bb6f70fc3d4b793cde6
nitesh_4u@yahoo.com <123456@sS>
2019-04-25 19:28:56 -0400

    After Login is perfect URL FIle

--- app/src/main/java/refresh/towson/com/tracs/AppConfig.java
+++ app/src/main/java/refresh/towson/com/tracs/AppConfig.java
@@ -1,8 +1,9 @@
 package refresh.towson.com.tracs;
 
-public class AppConfig {
+public  class AppConfig {
 
-       public static String URL_LOGIN = "http://192.168.0.102/applogin2.php";
+         String ServerLoginURL = "http://10.128.183.189/applogin2.php";
+
 
     }
 
