878879014d1c9a7af4ae9232f951f63bf5a2e2db
nitesh_4u@yahoo.com <123456@sS>
2019-04-25 19:29:36 -0400

    After Login is perfect Login Function file

--- app/src/main/java/refresh/towson/com/tracs/MainActivity.java
+++ app/src/main/java/refresh/towson/com/tracs/MainActivity.java
@@ -2,22 +2,25 @@ package refresh.towson.com.tracs;
 
 import android.os.Bundle;
 import android.app.ProgressDialog;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
-
+import android.os.AsyncTask;
 import android.view.View;
-
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
     EditText email;
     EditText password;
     Button login;
-    @Override
+    ProgressDialog progressDialog;
+    public static final String UserEmail = "";
+    String finalResult ;
+    HashMap<String,String> hashMap = new HashMap<>();
+    HttpParse httpParse = new HttpParse();
+
     protected void onCreate(Bundle savedInstanceState) {
 
 
@@ -32,22 +35,100 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String e=email.getText().toString();
                 String p=password.getText().toString();
-                if(e.equals("111")&&p.equals("pass")){
-                    Toast.makeText(getApplicationContext(),
-                            "Redirecting...",Toast.LENGTH_SHORT).show();
-                    Intent i = new Intent(getApplicationContext(),MainScreen.class);
-                    startActivity(i);
-                    setContentView(R.layout.activity_main_screen);
-                }
-                else
-                {
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
-                    login.setEnabled(false);
-                }
+                UserLoginFunction(e,p);
             }
         });
     }
 
+
+    public void UserLoginFunction(final String email, final String password){
+
+        class UserLoginClass extends AsyncTask<String,Void,String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+
+                progressDialog = ProgressDialog.show(MainActivity.this,"Loading Data",null,true,true);
+            }
+
+            @Override
+            protected void onPostExecute(String httpResponseMsg) {
+
+                super.onPostExecute(httpResponseMsg);
+
+                progressDialog.dismiss();
+
+                if(httpResponseMsg.equalsIgnoreCase("athlete")){
+
+                    finish();
+
+                    Intent intent = new Intent(MainActivity.this, MainScreen.class);
+
+                    intent.putExtra(UserEmail,email);
+
+                    startActivity(intent);
+
+                }
+                else if(httpResponseMsg.equalsIgnoreCase("strength_coach")){
+
+                    finish();
+
+                    Intent intent = new Intent(MainActivity.this, Strength_Coach.class);
+
+                    intent.putExtra(UserEmail,email);
+
+                    startActivity(intent);
+
+                }
+                else if(httpResponseMsg.equalsIgnoreCase("team_coach")){
+
+                    finish();
+
+                    Intent intent = new Intent(MainActivity.this, Team_Coach.class);
+
+                    intent.putExtra(UserEmail,email);
+
+                    startActivity(intent);
+
+                }
+
+                else{
+
+                    Toast.makeText(MainActivity.this,httpResponseMsg,Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+            protected String doInBackground(String... params) {
+
+                hashMap.put("email",params[0]);
+
+                hashMap.put("password",params[1]);
+                AppConfig app=new AppConfig();
+
+                finalResult = httpParse.postRequest(hashMap, app.ServerLoginURL);
+
+                return finalResult;
+            }
+        }
+
+        UserLoginClass userLoginClass = new UserLoginClass();
+
+        userLoginClass.execute(email,password);
+    }
+
+
+
+  /*  public void loginAgain(){
+        Toast.makeText(getBaseContext(),"Try Again",Toast.LENGTH_LONG).show();
+        login.setEnabled(true);
+    }
+
+    public void onLoginSuccess(){
+        login.setEnabled(true);
+        finish();
+    }
 
     public void login(){
         if(!validate()){
@@ -60,14 +141,19 @@ public class MainActivity extends AppCompatActivity {
         boolean valid=true;
         String e=email.getText().toString();
         String p=password.getText().toString();
+        valid = validate(e,p);
+        return valid;
+    }
+
+    public boolean validate(String e, String p){
         if( e==null){
             email.setError("enter valid email");
-            valid=false;
+            return false;
         }
 
         if(p==null){
             password.setError("Invalid");
-        valid=false;
+            return false;
         }
 
         else
@@ -77,17 +163,9 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
-    return valid;
-    }
+        return true;
+    } */
 
 
-    public void loginAgain(){
-        Toast.makeText(getBaseContext(),"Try Again",Toast.LENGTH_LONG).show();
-        login.setEnabled(true);
-    }
 
-    public void onLoginSuccess(){
-        login.setEnabled(true);
-        finish();
-    }
 }
